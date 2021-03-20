f7a095f045f21fba65123aa8f61b647ffe92e290
jemidamanik23 <63108354+jemidamanik23@users.noreply.github.com>
2021-03-20 23:09:47 +0700

    dua

--- Cek Nilai PHP/proses.php
+++ Cek Nilai PHP/proses.php
@@ -1,57 +1,46 @@
- <!DOCTYPE HTML>  
+<!DOCTYPE html>
 <html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>  
+<body>
+
+<h2>HTML Forms Cek Nilai</h2>
+
+<form action="proses.php" method="post">
+  <label for="nilai">Masukkan Nilai Angka</label><br>
+  <input type="text" id="nilai" name="nilai" ><br>
+   <br><br>
+  <input type="submit" value="Submit">
+</form> 
 
 <?php
-
-$usernameErr = $passwordErr = "";
-$username = $password = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   if (empty($_POST["username"])) {
-      $usernameErr = "Username is required";
-   }
-   elseif (strlen($_POST["username"]) !== 6) {
-         $usernameErr = "Username harus 6 karakter";
-      }  
-      else {
-    $username = test_input($_POST["username"]);
-   }    
-}
-
-if (empty($_POST["password"])) {
-    $passwordErr = "Password is required";
-} else {
-  $password = test_input($_POST["password"]);
-}
-
-  
-
-
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
+
+$nilai = $_POST["nilai"];
+
+if ($nilai > 100) {
+    $tertulis = "Nilai Tidak Valid";
+} elseif($nilai >= 90){
+    $tertulis = "Terpuji";
+} elseif($nilai >= 80){
+    $tertulis = "Sangat Memuaskan";
+} elseif($nilai >= 60){
+    $tertulis = "Memuaskan ";
+} elseif($nilai < 60){
+    $tertulis = "Tidak Lulus";
+} else {
+    $tertulis = "Data Belum Dimasukkan dengan benar";
+}
+
+echo "Nilai Angka anda: $nilai<br>";
+echo "Nilai tertulis: $tertulis<br><br>";
+echo "<b>Note : Jika Terdapat error Undefined index: nilai in C:\xampp\htdocs\Frontend\proses.php on line 22 , itu berarti anda belum memasukkan nilai dengan benar. Silahkan masukkan nilai anda pada form dan submit!</b>";
+
+
 ?>
-<h2>PHP Form PHP</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Username: <input type="text" name="username">
-  <span class="error">* <?php echo $usernameErr;?></span>
-  <br><br>
-  Password: <input type="text" name="password">
-  <span class="error">* <?php echo $passwordErr;?></span>
-  <br><br>
-  
-  <input type="submit" name="submit" value="Submit">  
-</form>
 
 </body>
 </html>
